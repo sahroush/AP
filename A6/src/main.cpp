2b035b35b3e048bb386f0455c79545fe5ee7d453
Soroush Sahraei <soroush.sahraei@gmail.com>
2023-05-16 13:16:20 +0330

    Add main

---
+++ A6/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void proccess_input(){
+    string line;
+    while(getline(cin, line)){
+        cout << "Banana" << endl;
+    }
+}
+
+int main(){
+    string line;
+    proccess_input();
+    return 0;
+}
